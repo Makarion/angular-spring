079d56423e49903d81e538ae0ce6e1505ba3902f
Kamil Makarewicz <kamilmakarewicz1996@gmail.com>
2022-09-20 07:46:29 +0200

    add integration tests

--- src/main/java/com/makarewk/angulartutorial/webservice/controllers/EquipmentController.java
+++ src/main/java/com/makarewk/angulartutorial/webservice/controllers/EquipmentController.java
@@ -13,7 +13,6 @@ import java.util.Optional;
 @RequestMapping(path="/equipment")
 @CrossOrigin(origins = "http://localhost:4200")
 public class EquipmentController {
-
     private final EquipmentRepository equipmentRepository;
 
     public EquipmentController(EquipmentRepository equipmentRepository) {
